1d52042d08cf7cc41530034bd2b5100156834633
kaka1234556 <80517682+kaka1234556@users.noreply.github.com>
2023-04-12 20:45:30 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+import os
+
+from flask import Flask, request, jsonify, render_template, redirect, url_for,send_file
+from faker import Faker
+from twilio.jwt.access_token import AccessToken
+from twilio.jwt.access_token.grants import SyncGrant
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+fake = Faker()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/token')
+def generate_token():
+    TWILIO_ACCOUNT_SID = 'ACb839d91885313fb1184fd5937469b31c'
+    TWILIO_SYNC_SERVICE_SID = 'ISb042f3e13ee3e8c36a73119814152d28'
+    TWILIO_API_KEY = 'SK6f8401bac6ba11fea2718e2d1a9beaef'
+    TWILIO_API_SECRET = 'bM2GFMgf6XVLaQ0JMyunAokaFr643eDa'
+
+    username = request.args.get('username', fake.user_name())
+    token = AccessToken(TWILIO_ACCOUNT_SID, TWILIO_API_KEY, TWILIO_API_SECRET, identity=username)
+    # create a Sync grant and add to token
+    sync_grant_access = SyncGrant(TWILIO_SYNC_SERVICE_SID)
+    token.add_grant(sync_grant_access)
+    return jsonify(identity=username, token=token.to_jwt().decode())
+
+@app.route('/', methods=['POST'])
+def download_text():
+    text_from_notepad=request.form['text']
+    with open("work.txt","w") as f:
+        f.write(text_from_notepad)
+
+
+    path_to_store_txt="work.file.txt"
+
+    return  send_file(path_to_store_txt,as_attachment=True)
+        
+
+    
+
+    
+
+
+if __name__ == "__main__":
+    app.run(host='localhost', port='5001', debug=True)
